c70ad7bc44589b69257af9fef2b9eb02102279a1
thetribalme <thetribaleng@gmail.com>
2023-07-03 15:04:44 +0200

    task is done

--- task_2.py
+++ task_2.py
@@ -2,3 +2,15 @@
 Необходимо написать функцию, которая берет массив и перемещает все нули в конец,
 сохраняя порядок остальных элементов
 '''
+random_array = [0, 1, 0, 3, 9, 0, 5, 0, 13, 56, 0, 4, 7, 8]   # for example
+
+
+def move_zeroes(array):
+    zeroes_count = array.count(0)
+    for i in range(zeroes_count):
+        array.remove(0)
+    array.extend([0 for i in range(zeroes_count)])
+    return array
+
+
+print(move_zeroes(random_array))
